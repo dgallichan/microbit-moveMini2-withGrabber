e113ac667c837029eb20b3279a66fd4c790cca2e
Daniel Gallichan <dgallichan@yahoo.com>
2022-09-21 15:20:36 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://dgallichan.github.io/microbit-movemini2-withgrabber/](https://dgallichan.github.io/microbit-movemini2-withgrabber/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/dgallichan/microbit-movemini2-withgrabber** and import
+
+## Edit this project ![Build status badge](https://github.com/dgallichan/microbit-movemini2-withgrabber/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/dgallichan/microbit-movemini2-withgrabber** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/dgallichan/microbit-movemini2-withgrabber/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,76 @@
+radio.onReceivedValueDeprecated(function (name, value) {
+    if (name == "Roll") {
+        RawRoll = value
+        MappedRoll = pins.map(
+        RawRoll,
+        -90,
+        90,
+        0,
+        180
+        )
+        RollLeft = MappedRoll
+        RollRight = MappedRoll
+    }
+    if (name == "Pitch") {
+        RawPitch = value
+        MappedPitch = pins.map(
+        RawPitch,
+        90,
+        -90,
+        0,
+        180
+        )
+        PitchLeft = MappedPitch
+        PitchRight = 180 - MappedPitch
+    }
+    LeftOutput = (PitchLeft + RollLeft) / 2
+    RightOutput = (PitchRight + RollRight) / 2
+    if (RawPitch == 0 && RawRoll == 0) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else {
+        pins.servoWritePin(AnalogPin.P1, LeftOutput)
+        pins.servoWritePin(AnalogPin.P2, RightOutput)
+    }
+    if (name == "Grabber") {
+        RawGrabber = value
+        if (RawGrabber == -999) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        } else {
+            MappedGrabber = pins.map(
+            RawGrabber,
+            -90,
+            90,
+            0,
+            180
+            )
+            pins.servoWritePin(AnalogPin.P0, MappedGrabber)
+        }
+    }
+})
+let MappedGrabber = 0
+let RawGrabber = 0
+let RightOutput = 0
+let LeftOutput = 0
+let PitchRight = 0
+let PitchLeft = 0
+let MappedPitch = 0
+let RawPitch = 0
+let RollRight = 0
+let RollLeft = 0
+let MappedRoll = 0
+let RawRoll = 0
+radio.setGroup(1)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.forever(function () {
+    basic.pause(100)
+})
+basic.forever(function () {
+	
+})
